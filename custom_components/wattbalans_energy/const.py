094bd7688fa2d354506ddfe993a2db5dabe382b5
Virtruvius <alex.hut@virtruvian.com>
2026-07-08 09:18:14 +0200

    feat: add integration constants

---
+++ custom_components/wattbalans_energy/const.py
@@ -0,0 +1,22 @@
+"""Constants for WattBalans Energy Management."""
+
+DOMAIN = "wattbalans_energy"
+NAME = "WattBalans Energy Management"
+
+CONF_FEATURES = "features"
+
+FEATURE_SOLAR = "solar"
+FEATURE_GRID = "grid"
+FEATURE_BATTERY = "battery"
+FEATURE_EV_CHARGING = "ev_charging"
+FEATURE_DYNAMIC_TARIFF = "dynamic_tariff"
+FEATURE_CONTROLLABLE_LOADS = "controllable_loads"
+
+DEFAULT_FEATURES = {
+    FEATURE_SOLAR: True,
+    FEATURE_GRID: True,
+    FEATURE_BATTERY: False,
+    FEATURE_EV_CHARGING: False,
+    FEATURE_DYNAMIC_TARIFF: False,
+    FEATURE_CONTROLLABLE_LOADS: False,
+}
